9a14c6ff477ebf48dee6e73b5d311cef99322f8f
adipa <aditipatil110899@gmail.com>
2020-06-04 22:20:50 +0530

    Add User authorization

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@
 
 import * as React from "react";
 import { Admin } from 'ra-core';
+import dataProvider from 'api/dataProvider'
 
 import {Login, Logout, authProvider} from './user';
+
+const httpClient = (url, options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+    }
+    const token = localStorage.getItem('token');
+    options.headers.set('Authorization', `Bearer ${token}`);
+    return fetchUtils.fetchJson(url, options);
+};
+const dataProvider = springbootrest('http://localhost:3000/api', httpClient);
 
 const App = () => (
     <Admin loginPage={Login} logoutButton={Logout} authProvider={authProvider} />
