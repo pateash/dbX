2db054bdb12b8e6d4f7bf518e530c99d9290b37c
dev681999 <dev681999@gmail.com>
2020-06-18 13:30:53 +0530

    server url update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 const localServerBaseUrl = 'http://localhost:8080';
-const openShiftServerBaseUrl = 'http://dbx-dbx.192.168.42.23.nip.io';
+// const openShiftServerBaseUrl = 'http://dbx-dbx.192.168.42.23.nip.io';
+const openShiftServerBaseUrl = 'http://dbx-dbx.apps.123.252.203.195.nip.io/';
 
 const serverBaseUrl = process.env.NODE_ENV === "development" ? localServerBaseUrl : openShiftServerBaseUrl;
 
